16f3a996219e6d4ba1e24f3f5343b4461fc65eb8
root <root@ip-172-31-19-220.ap-southeast-1.compute.internal>
2018-11-12 09:32:28 +0000

    adding mystring.py

---
+++ pythonscripts/mystring.py
@@ -0,0 +1,15 @@
+s = input("Give me a number: ")
+s = int(s)
+print(type(s))
+
+#print (s[::-1])
+#s = s[::-1]
+#print(s)
+
+
+#s[4] = "l"
+#print(help(s.replace))
+#z = s.replace("L","l",3)
+
+#print(s.rjust(25,"#"))
+
